809e7101eccf1865d72a7143df6be64aff6179f6
Timothy Pearson <timo.555@icloud.com>
2020-01-17 16:39:48 +0100

    Hammerspoon config location change. Added journal snippet

--- init.lua
+++ init.lua
@@ -2,12 +2,13 @@
 
 
 -- reload this config file on save
-local myWatcher = hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", hs.reload):start()
+local myWatcher = hs.pathwatcher.new(os.getenv("HOME") .. "/www/dotfiles/init.lua", hs.reload):start()
+-------------------------------------------------------
+
 
 -- hs.hotkey.bind({"cmd", "alt", "ctrl"}, "W", function()
 --   hs.alert.show("testing", hs.alert.defaultStyle, hs.screen.mainScreen(), 1)
 -- end)
-
 
 -- d: date snippet
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "d", function()
@@ -15,6 +16,30 @@ hs.hotkey.bind({"cmd", "alt", "ctrl"}, "d", function()
   myDate = os.date("%Y%m%d") .. ".md"
   hs.pasteboard.setContents(myDate)
 end)
+-------------------------------------------------------
+
+-- j: journal timestamp snippet in the form of
+-- ---
+-- year: 2020
+-- month: January
+-- day: Thu 02
+-- ---
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "j", function()
+  hs.pasteboard.clearContents()
+  year = os.date("%Y")
+  month = os.date("%B")
+  day = os.date("%a")
+  dayNumber = os.date("%d")
+
+  toPaste = [[---
+year: ]] .. year .. [[ 
+month: ]] .. month .. [[ 
+day: ]] .. day .. " " .. dayNumber .. [[ 
+---]]
+
+  hs.pasteboard.setContents(toPaste)
+end)
+-------------------------------------------------------
 
 -- t: today snippet
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "t", function()
@@ -29,3 +54,5 @@ hs.hotkey.bind({"cmd", "alt", "ctrl"}, "t", function()
 ]]
   hs.pasteboard.setContents(toPaste)
 end)
+-------------------------------------------------------
+
