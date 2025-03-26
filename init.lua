d43e6a44fe47720c7c4548551e6035f8c22147a2
Timothy Pearson <timo.555@icloud.com>
2025-03-26 10:41:40 -0700

    Hammerspoon conf changes

--- init.lua
+++ init.lua
@@ -1,26 +1,26 @@
--- Hammerspoon config file
+-- # Hammerspoon config file
 
-
--- Global variables
-hyper =       {"cmd", "alt", "ctrl"}
-shift_hyper = {"cmd", "alt", "ctrl", "shift"}
 
 -- reload this config file on save
 local myWatcher = hs.pathwatcher.new(os.getenv("HOME") .. "/www/dotfiles/init.lua", hs.reload):start()
 -------------------------------------------------------
+
+-- ## Global variables
+hyper =       {"cmd", "alt", "ctrl"}
+shift_hyper = {"cmd", "alt", "ctrl", "shift"}
+option_shift = {"option", "shift"}
 
 -- applescript to paste contents of pasteboard
 pastescript = [[ tell application "System Events"
   delay .4
   keystroke "v" using command down
 end tell ]]
+-------------------------------------------------------
 
 
--- hs.hotkey.bind(hyper, "W", function()
---   hs.alert.show("testing", hs.alert.defaultStyle, hs.screen.mainScreen(), 1)
--- end)
+-- ## Paste helpers 
 
--- d: date snippet
+-- hyper d: date snippet
 hs.hotkey.bind(hyper, "d", function()
   hs.pasteboard.clearContents()
   myDate = os.date("%Y%m%d") .. ".md"
@@ -29,28 +29,7 @@ hs.hotkey.bind(hyper, "d", function()
 end)
 -------------------------------------------------------
 
-
--- o: open my journal sparseimage
-hs.hotkey.bind(hyper, "o", function()
-  openJourn = [[do shell script "open ']] .. os.getenv("HOME") .. [[/Library/Mobile Documents/com~apple~CloudDocs/MyDocs/journ.sparseimage'"]]
-  hs.osascript.applescript(openJourn)
-
-end)
-
--------------------------------------------------------
-
-
--- k: open my daily tracKing sheet
-hs.hotkey.bind(hyper, "k", function()
-  openSheet = [[do shell script "open ']] .. os.getenv("HOME") .. [[/Library/Mobile Documents/com~apple~Numbers/Documents/Daily.numbers'"]]
-  hs.osascript.applescript(openSheet)
-
-end)
-
--------------------------------------------------------
-
-
--- j: journal timestamp snippet in the form of
+-- hyper j: journal timestamp snippet in the form of
 -- ---
 -- year: 2020
 -- month: January
@@ -77,7 +56,7 @@ time: ]] .. time .. [[
 end)
 -------------------------------------------------------
 
--- t: today snippet
+-- hyper t: today snippet
 hs.hotkey.bind(hyper, "t", function()
   hs.pasteboard.clearContents()
   day = os.date("%A")
@@ -92,13 +71,54 @@ end)
 -------------------------------------------------------
 
 
--- m: open my work time tracking sheet for Alita 2025
+-- ## Open helpers
+
+-- hyper m: open my work time tracking sheet for Alita 2025
 -- Money starts with m. I get paid for each entry in this doc
 hs.hotkey.bind(hyper, "m", function()
   openSheet = [[do shell script "open ']] .. os.getenv("HOME") .. [[/Library/Mobile Documents/com~apple~CloudDocs/Work/000 time tracking/Alita time 2025.numbers'"]]
   hs.osascript.applescript(openSheet)
 
 end)
+-------------------------------------------------------
 
+-- hyper k: open my daily tracKing sheet
+hs.hotkey.bind(hyper, "k", function()
+  openSheet = [[do shell script "open ']] .. os.getenv("HOME") .. [[/Library/Mobile Documents/com~apple~Numbers/Documents/Daily.numbers'"]]
+  hs.osascript.applescript(openSheet)
+
+end)
+-------------------------------------------------------
+
+-- hyper o: open my journal sparseimage
+hs.hotkey.bind(hyper, "o", function()
+  openJourn = [[do shell script "open ']] .. os.getenv("HOME") .. [[/Library/Mobile Documents/com~apple~CloudDocs/MyDocs/journ.sparseimage'"]]
+  hs.osascript.applescript(openJourn)
+
+end)
+-------------------------------------------------------
+
+-- option shift f: open/focus Firefox
+hs.hotkey.bind(option_shift, "f", function()
+  hs.application.launchOrFocus("/Applications/Firefox.app")
+end)
+-------------------------------------------------------
+
+-- option shift c: open/focus Chrome
+hs.hotkey.bind(option_shift, "c", function()
+  hs.application.launchOrFocus("/Applications/Google Chrome.app")
+end)
+-------------------------------------------------------
+
+-- option shift v: open/focus Visual Studio Code
+hs.hotkey.bind(option_shift, "v", function()
+  hs.application.launchOrFocus("/Applications/Visual Studio Code.app")
+end)
+-------------------------------------------------------
+
+-- option shift s: open/focus Sublime Text
+hs.hotkey.bind(option_shift, "s", function()
+  hs.application.launchOrFocus("/Applications/Sublime Text.app")
+end)
 -------------------------------------------------------
 
